78abff9e9db9b2fd09173acd3be0c170d347af0b
tushargarg1705 <tushar.garg@netsolutions.com>
2023-02-20 15:37:50 +0530

    assignments

---
+++ demo.py
@@ -0,0 +1,7 @@
+print("hello world")
+print('tushar')
+
+
+
+for i in range(10):
+    print(i)
---
+++ prime.py
@@ -0,0 +1,20 @@
+start_no=int(input('Enter the number from which you want to start:'))
+end_no= int(input('Enter the number upto which you want to check:'))
+
+
+def prime(start_no, end_no):
+
+    for num in range(start_no,end_no +1 ):
+        if num>1:
+            for i in range(2,num):
+                if num%i ==0:
+                    break;
+            
+            else:
+                print('Prime numbers are:',num)
+
+
+
+
+prime(start_no, end_no)
+    
